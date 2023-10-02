a6ce8a8fcdb154092d442347c8d9b2af95721d73
Oleg Yudin <puzatin@gmail.com>
2023-10-02 13:42:37 +0600

    Update index.js

--- packages/wagmi/exports/index.js
+++ packages/wagmi/exports/index.js
@@ -1,12 +1,10 @@
-import { Web3ModalOptions } from '../src/client.js'
-import { Web3Modal } from '../src/client.js'
-import { VERSION } from '../src/utils/constants.js'
+import { Web3Modal } from '../src/client.js';
+import { VERSION } from '../src/utils/constants.js';
 
-export { Web3ModalOptions } from '../src/client.js'
-export { EIP6963Connector } from '../src/connectors/EIP6963Connector.js'
-export { defaultWagmiConfig } from '../src/utils/defaultWagmiCoreConfig.js'
-export { walletConnectProvider } from '../src/utils/provider.js'
+export { EIP6963Connector } from '../src/connectors/EIP6963Connector.js';
+export { defaultWagmiConfig } from '../src/utils/defaultWagmiCoreConfig.js';
+export { walletConnectProvider } from '../src/utils/provider.js';
 
-export function createWeb3Modal(options: Web3ModalOptions) {
-  return new Web3Modal({ ...options, _sdkVersion: `html-wagmi-${VERSION}` })
+export function createWeb3Modal(options) {
+  return new Web3Modal({ ...options, _sdkVersion: 'html-wagmi-' + VERSION });
 }
