ee7f16e077f43560004b1bfdf6a84c55f2420621
Oleg Yudin <puzatin@gmail.com>
2023-10-02 13:37:47 +0600

    Update index.js

--- packages/wagmi/exports/index.js
+++ packages/wagmi/exports/index.js
@@ -1,4 +1,4 @@
-import type { Web3ModalOptions } from '../src/client.js'
+import { Web3ModalOptions } from '../src/client.js'
 import { Web3Modal } from '../src/client.js'
 import { VERSION } from '../src/utils/constants.js'
 
